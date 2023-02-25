28de36ab4c055ccf11370a6df590c2033baed620
Luanaap <57630311+Luanaap@users.noreply.github.com>
2023-02-25 00:08:38 -0300

    Criando repositorio de Sistema de Login

---
+++ SistemaLogin/CadastroUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    static class CadastroUsuarios
+    {
+        private static Usuario[] usuarios = 
+        { 
+            new Usuario(){ Nome = "Luana", Senha="ABC123" },
+            new Usuario(){ Nome = "Giovanna", Senha="123ABC"},
+            new Usuario(){ Nome = "Lisa", Senha= "abcd"}
+        
+        };
+
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado; }
+            private set { _userLogado = value; }
+        }
+
+        public static bool Login(string nome, string senha)
+        {
+            foreach(Usuario usuario in usuarios)
+            {
+                if(usuario.Nome == nome && usuario.Senha == senha)
+                {
+                    UsuarioLogado = usuario;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
